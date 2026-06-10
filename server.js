28db39865d26f8681401b83e075cd4b19a2cf439
flyworkteam <flyworkteam@gmail.com>
2026-06-10 16:24:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const geminiRoutes = require('./routes/geminiRoutes');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api/webhooks', webhookRoutes);
 // HTTP sunucusu oluştur (Socket.IO için gerekli)
